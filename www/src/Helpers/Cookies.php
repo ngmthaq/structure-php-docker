89fb2c7ef2eb5ee0885289a533c42396abf3d582
Nguyen Manh Thang <nguyenmanhthang2000.fb@gmail.com>
2023-07-01 12:26:20 +0700

    Update helper

--- www/src/Helpers/Cookies.php
+++ www/src/Helpers/Cookies.php
@@ -4,5 +4,39 @@ namespace Src\Helpers;
 
 class Cookies
 {
-    //
+    /**
+     * Set cookies
+     * 
+     * @param string $name
+     * @param string $value
+     * @param int $time
+     * @return void
+     */
+    public static function set(string $name, string $value, int $time)
+    {
+        setcookie($name, $value, $time);
+    }
+
+    /**
+     * Set cookies
+     * 
+     * @param string $name
+     * @return void
+     */
+    public static function remove(string $name)
+    {
+        unset($_COOKIE[$name]);
+        setcookie($name, "", -1);
+    }
+
+    /**
+     * Get cookies
+     * 
+     * @param string $name
+     * @return string|null
+     */
+    public static function get(string $name)
+    {
+        return isset($_COOKIE[$name]) ? $_COOKIE[$name] : null;
+    }
 }
--- www/src/Helpers/Hash.php
+++ www/src/Helpers/Hash.php
@@ -4,5 +4,26 @@ namespace Src\Helpers;
 
 class Hash
 {
-    //
+    /**
+     * Hash a string using md5 function
+     * 
+     * @param string $plain
+     * @return string
+     */
+    public static function make(string $plain)
+    {
+        return md5($plain);
+    }
+
+    /**
+     * Compare plain text and hash text
+     * 
+     * @param string $plain
+     * @param string $hashed
+     * @return bool
+     */
+    public static function check(string $plain, string $hashed)
+    {
+        return self::make($plain) === $hashed;
+    }
 }
