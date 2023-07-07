a0abc2f4c66e3f406172723200da6cf3f1b3648f
Nguyen Manh Thang <nguyenmanhthang2000.fb@gmail.com>
2023-07-07 07:50:02 +0700

    Tmp handle database connection

--- www/src/Controllers/HomeController.php
+++ www/src/Controllers/HomeController.php
@@ -2,16 +2,10 @@
 
 namespace Src\Controllers;
 
-use Error;
-use PDO;
-use Src\Helpers\Dev;
-
 class HomeController extends BaseController
 {
     public function index()
     {
-        $conn = new PDO('mysql:host=mysql-service;dbname=php;port=3306', "root", "root");
         $this->renderView("pages.home");
-        Dev::console($conn);
     }
 }
---
+++ www/src/Helpers/Database.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Src\Helpers;
+
+use PDO;
+
+class Database
+{
+    /**
+     * Database host
+     */
+    protected string $host;
+
+    /**
+     * Database port
+     */
+    protected string $port;
+
+    /**
+     * Database name
+     */
+    protected string $dbname;
+
+    /**
+     * Database username
+     */
+    protected string $username;
+
+    /**
+     * Database password
+     */
+    protected string $password;
+
+    /**
+     * SQL string template
+     */
+    protected string $sql;
+
+    /**
+     * Binding param values
+     */
+    protected array $params;
+
+    /**
+     * Database connection
+     */
+    protected PDO $conn;
+
+    public function __construct()
+    {
+        $this->host = $_ENV["DB_HOST"];
+        $this->port = $_ENV["DB_PORT"];
+        $this->username = $_ENV["DB_USERNAME"];
+        $this->password = $_ENV["DB_PASSWORD"];
+        $this->dbname = $_ENV["DB_NAME"];
+        $this->sql = "";
+        $this->params = [];
+        $this->connect();
+    }
+
+    /**
+     * Connect to database server
+     * 
+     * @return void
+     */
+    protected function connect()
+    {
+        $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->dbname . ";port=" . $this->port;
+        $conn = new PDO($dsn, $this->username, $this->password);
+        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->conn = $conn;
+    }
+
+    /**
+     * Set SQL string template
+     * 
+     * @param string $sql sql template
+     * @return void
+     */
+    public function setSql(string $sql)
+    {
+        $this->sql = $sql;
+    }
+
+    /**
+     * Binding parameter
+     * 
+     * @param string $binding binding key
+     * @param string|int $value binding value
+     * @param int $type binding type (PDO::PARAM_STR | PDO::PARAM_INT)
+     * @return void
+     */
+    public function setParam(string $binding, string|int $value, int $type = PDO::PARAM_STR)
+    {
+        $this->params[] = compact("binding", "value", "type");
+    }
+
+    /**
+     * Begin transaction
+     * 
+     * @return bool
+     */
+    public function begin()
+    {
+        return $this->conn->beginTransaction();
+    }
+
+    /**
+     * Commit transaction
+     * 
+     * @return bool
+     */
+    public function commit()
+    {
+        return $this->conn->commit();
+    }
+
+    /**
+     * Roll back transaction
+     * 
+     * @return bool
+     */
+    public function rollBack()
+    {
+        return $this->conn->rollBack();
+    }
+}
